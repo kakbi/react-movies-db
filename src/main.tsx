16d1bb32aac8cf3188a4d7d93c65f438b4ea8908
Vadym Chebotar <dramvoda@gmail.com>
2026-01-30 21:51:56 +0200

    feat: add ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,48 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-import { createRoot } from 'react-dom/client';
-import './index.scss';
-import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import About from './features/About/About.tsx';
-import Movies from './features/Movies/Movies.tsx';
-import { Provider } from 'react-redux';
-import store from './store.ts';
-import Home from './features/Home/Home.tsx';
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./features/About/About.tsx";
+import Movies from "./features/Movies/Movies.tsx";
+import { Provider } from "react-redux";
+import store from "./store.ts";
+import Home from "./features/Home/Home.tsx";
+import { ErrorBoundary } from "./ErrorBoundary.tsx";
 
 function AppEntryPoint() {
-    return (
-        <Provider store={store}>
-            <App />
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Provider>
+  );
 }
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <AppEntryPoint />,
-        children: [
-            { path: '/', element: <Home /> },
-            {
-                path: '/about',
-                element: <About />,
-            },
-            {
-                path: '/movies',
-                element: <Movies />,
-            },
-        ],
-    },
+  {
+    path: "/",
+    element: <AppEntryPoint />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/movies",
+        element: <Movies />,
+      },
+    ],
+  },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router} />,
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
 );
+
